03c951dc7eef18c73394306fe1f5fee2bea916f7
Iliescu Mihail Dorin <31242262+Iliescu-Dorin@users.noreply.github.com>
2022-08-01 22:01:27 +0300

    My Projects Repo First Commit
    Added my details

--- .history/script_20220801215800.js
+++ .history/script_20220801215800.js
@@ -1,4 +1,4 @@
-const username = 'Iliescu-Dorin';
+const username = '2kabhishek';
 const maxPages = 2;
 const repoList = document.querySelector('.repo-list');
 const reposSection = document.querySelector('.repos');
